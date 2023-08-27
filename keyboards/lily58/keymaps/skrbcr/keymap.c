400b521f0ab2f22c1fb8732f1e71d2c51f467e41
skrbcr <90087433+skrbcr@users.noreply.github.com>
2023-08-27 16:56:31 +0900

    Update keymap.c

--- keyboards/lily58/keymaps/skrbcr/keymap.c
+++ keyboards/lily58/keymaps/skrbcr/keymap.c
@@ -8,100 +8,43 @@ enum layer_number {
   _ADJUST,
 };
 
-enum {
-  TD_SHIFT_TAB,
+enum custom_keycodes {
+  C_LOWER = SAFE_RANGE,
+  C_RAISE,
 };
 
-//Tap Dance Definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-  //Tap once for Esc, twice for Caps Lock
-  /* [TD_ESC_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS) */
-  [TD_SHIFT_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_LEFT_SHIFT, KC_TAB)
-};
+/* enum { */
+/*   TD_ZKHK,  // 全角半角 */
+/* }; */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* QWERTY
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
- */
-
  [_QWERTY] = LAYOUT(
- XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  KC_ESC,    JP_Q,    JP_W,    JP_E,    JP_R,    JP_T,                     JP_Y,    JP_U,    JP_I,    JP_O,    JP_P, KC_BSPC,
-  JP_ZKHK,   JP_A,    JP_S,    JP_D,    JP_F,    JP_G,                     JP_H,    JP_J,    JP_K,    JP_L, KC_SCLN,  KC_DEL,
-  KC_LEFT_GUI, JP_Z,  JP_X,    JP_C,    JP_V,    JP_B, XXXXXXX,   XXXXXXX, JP_N,  JP_M, KC_COMMA, KC_DOT, KC_SLSH, KC_LEFT_ALT,
-                        KC_LEFT_CTRL, MO(_RAISE), TD(TD_SHIFT_TAB), KC_SPC, KC_ENT, KC_LEFT_CTRL, MO(_LOWER), XXXXXXX
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+   KC_ESC,    JP_Q,    JP_W,    JP_E,    JP_R,    JP_T,                      JP_Y,    JP_U,    JP_I,    JP_O,    JP_P, KC_BSPC,
+  JP_MHEN,  JP_A,    JP_S,    JP_D,    JP_F,    JP_G,                      JP_H,    JP_J,    JP_K,    JP_L, KC_SCLN,  KC_DEL,
+  KC_LWIN,    JP_Z,    JP_X,    JP_C,    JP_V,    JP_B, XXXXXXX, XXXXXXX,    JP_N,    JP_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LALT,
+                             XXXXXXX, C_RAISE, KC_LSFT,  KC_SPC,  KC_ENT, KC_LCTL, C_LOWER, XXXXXXX
 ),
-/* LOWER
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   -  |
- * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
- */
 [_LOWER] = LAYOUT(
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  _______, JP_1,    JP_2,    JP_3,    JP_4,    JP_5,                      JP_6,    JP_7,    JP_8,    JP_9,    JP_0,   _______,
-  _______, JP_MINS, JP_CIRC, JP_YEN,  JP_AT,   XXXXXXX,                   XXXXXXX, JP_LBRC, JP_RBRC, JP_BSLS, JP_COLN, _______,
+  _______, JP_1,    JP_2,    JP_3,    JP_4,    JP_5,                      JP_6,    JP_7,    JP_8,    JP_9,    JP_0,    _______,
+  JP_ZKHK, JP_MINS, JP_CIRC, JP_YEN,  JP_AT,   XXXXXXX,                   JP_PLUS, JP_LBRC, JP_RBRC, JP_BSLS, JP_COLN, _______,
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-                             _______, _______, _______, _______, _______,  _______, _______, _______
+                             _______, _______, _______, KC_TAB, _______,  _______, _______, _______
 ),
-/* RAISE
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------|      | Left | Down |  Up  |Right |      |
- * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
- * `-----------------------------------------/       /     \      \-----------------------------------------' | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI | |      |      |      |/       /         \      \ |      |      |      | `----------------------------'           '------''--------------------'
- */
-
 [_RAISE] = LAYOUT(
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     XXXXXXX,  KC_END, KC_HOME, XXXXXXX, XXXXXXX, _______,
-  _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                     KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, _______,
-  _______,  KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX, XXXXXXX, _______,
-                             _______, _______, _______,  _______, _______,  _______, _______, _______
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                   XXXXXXX,  KC_END, KC_HOME, XXXXXXX, XXXXXXX, _______,
+  JP_HENK,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                   KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, _______,
+  _______,  KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGDN, KC_PGUP, XXXXXXX, XXXXXXX, _______,
+                             _______, _______, _______, _______, _______, _______, _______, _______
 ),
-/* ADJUST
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|      |      |RGB ON| HUE+ | SAT+ | VAL+ |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
- *                   |      |      |      |/       /         \      \ |      |      |      |
- *                   `----------------------------'           '------''--------------------'
- */
-  [_ADJUST] = LAYOUT(
+[_ADJUST] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, KC_BRIU, KC_BRID,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, KC_PSCR,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, XXXXXXX, XXXXXXX,  KC_INS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                              _______, _______, _______, _______, _______,  _______, _______, _______
   )
 };
@@ -109,6 +52,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
+
+/* qk_tap_dance_action_t tap_dance_actions[] = { */
+/*     [TD_ZKHK] = ACTION_TAP_DANCE_DOUBLE(JP_MHEN, JP_HENK), */
+/* }; */
 
 //SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
 #ifdef OLED_ENABLE
@@ -156,33 +103,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #endif
     // set_timelog();
     }
-    /* switch (keycode) { */
-    /* case KC_LEFT_SHIFT: */
-    /*     if (record->event.pressed) { */
-    /*         tab_pressed = true; */
-    /*     } */
-    /*     else { */
-    /*         if (tab_pressed) { */
-    /*             register_code(KC_TAB); */
-    /*         } */
-    /*         unregister_code(KC_TAB); */
-    /*         unregister_code(KC_LEFT_SHIFT); */
-    /*         /1* else { *1/ */
-    /*         /1*     register_code(KC_LEFT_SHIFT); *1/ */
-    /*         /1*     unregister_code(KC_LEFT_SHIFT); *1/ */
-    /*         /1* } *1/ */
-    /*         tab_pressed = false; */
-    /*     } */
-    /*     return false; */
-    /* default: */
-    /*     if (record->event.pressed) { */
-    /*         if (tab_pressed) { */
-    /*             register_code(KC_LEFT_SHIFT); */
-    /*             tab_pressed = false; */
-    /*         } */
-    /*     } */
-    /*     break; */
-    /* } */
+    switch (keycode) {
+        case C_LOWER:
+            if (record->event.pressed) {
+                layer_on(_LOWER);
+            }
+            else {
+                layer_off(_LOWER);
+            }
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            return false;
+        case C_RAISE:
+            if (record->event.pressed) {
+                layer_on(_RAISE);
+            }
+            else {
+                layer_off(_RAISE);
+            }
+            update_tri_layer(_LOWER, _RAISE, _ADJUST);
+            return false;
+        default:
+            break;
+    }
     return true;
 }
 
